828592699ad8d02c3187707bba79c81753078275
fifou jack <gregvido17@gmail.com>
2022-10-15 09:12:48 +0200

    license apache 2.0

--- dwmExec.cc
+++ dwmExec.cc
@@ -1,5 +1,17 @@
 /*
-dwmExec.cc By GregVido (MissingNo.#9660)
+Copyright 2022 GregVido
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 #include <node.h>
