073dda1dc55037ac3d54550d3eb6edda943a4b05
fifou jack <gregvido17@gmail.com>
2022-11-12 18:20:32 +0100

    fix window animation

--- exemple.js
+++ exemple.js
@@ -51,7 +51,6 @@ function removeFrame(window) {
 
     window.hide();
 
-    executeDwm(HWND, PARAMS.FRAME, VALUE.FALSE);
     redraw(HWND, bounds.x, bounds.y, bounds.width, bounds.height, 0x0020);
 
     window.show();
