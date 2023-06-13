ca4a04d412287101540cb6af8d9226001c362e44
DeMineArchiver <minearchiver@gmail.com>
2023-06-13 15:30:02 +0300

    Rework JSDoc
    This commit reworks the JSDoc comments on methods and properties, supposedly making them clearer to understand

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,28 @@
 declare module "mica-electron" {
     import { BrowserWindow } from "electron";
 
+    // TODO: rework CSS colors
     // type HexColor = `#${string}`;
     // type RGBColor = `rgb(${number}, ${number}, ${number})`;
-    type CSSColor = string;
+    // type CSSColor = string;
 
     export class MicaBrowserWindow extends BrowserWindow {
+        /**
+         * Current theme value, represented by an integer
+         */
         public theme: number;
+        /**
+         * Current effect value, represented by an integer
+         */
         public effect: number;
+        /**
+         * Shows whether this window is using DWM or User32
+         */
         public useDWM: boolean;
+        /**
+         * Shows whether this window is frameless
+         */
         public hasFrameless: boolean;
-
 
         /**
          * Disable transparent for mica effect
@@ -23,147 +35,231 @@ declare module "mica-electron" {
         public enableMargin(): void;
 
         /**
-         * Apply MicaEffect (only windows 11)
+         * Apply the Mica effect (Windows 11 only)
          */
         public setMicaEffect(): void;
 
         /**
-         * Apply MicaTabbed Effect (only windows 11)
+         * Apply the Tabbed Mica effect (Windows 11 only)
          */
         public setMicaTabbedEffect(): void
         /**
-         * Apply Acrylic Effect (only windows 11)
+         * Apply the Acrylic effect (Windows 11 only)
          */
         public setMicaAcrylicEffect(): void;
 
         /**
-         * Apply dark theme to the electron app
+         * Change theme to dark
          */
         public setDarkTheme(): void;
 
         /**
-         * Apply light theme to the electron app
+         * Change theme to light
          */
         public setLightTheme(): void;
 
         /**
-        * Apply auto detection theme to the electron app
+        * Make theme automatically adapt to the system
         */
         public setAutoTheme(): void;
 
         /**
-        * Apply rounded corner to the electron app
+        * Set corner radius to normal (Windows 11 only)
         */
         public setRoundedCorner(): void;
 
         /**
-        * Apply rounded corner to the electron app
+        * Set corner radius to small (Windows 11 only)
         */
         public setSmallRoundedCorner(): void;
 
         /**
-        * Apply square corner to the electron app
+        * Set corner radius to 0 (Windows 11 only)
         */
         public setSquareCorner(): void;
 
         /**
-        * Set border color to the electron app
+        * Change window's border color
         */
-        public setBorderColor(color: CSSColor): void;
+        public setBorderColor(color: string): void;
 
         /**
-        * Set caption color to the electron app
+        * Change window's titlebar color
         */
-        public setCaptionColor(color: CSSColor): void;
+        public setCaptionColor(color: string): void;
 
         /**
-        * Set title text color to the electron app
+        * Change window's title text color
         */
-        public setTitleTextColor(color: CSSColor): void;
+        public setTitleTextColor(color: string): void;
 
         /**
-        * Apply transparent Effect (windows 7+)
+        * Apply the Transparent Effect (Windows 7+)
         */
         public setTransparent(): void;
 
         /**
-        * Apply blur Effect (windows 7+)
+        * Apply the Blur Effect (Windows 7+)
         */
         public setBlur(): void;
 
         /**
-        * Apply Acrylic Effect (windows 7+)
+        * Apply the Acrylic Effect (Windows 7+)
         */
         public setAcrylic(): void;
 
         /**
-         * Apply custom effect of SetWindowCompositionAttribute (windows 7+)
+         * Apply a custom effect of SetWindowCompositionAttribute (Windows 7+)
          */
-        public setCustomEffect(nAccentState: number, color: CSSColor, alpha: number): void;
+        public setCustomEffect(nAccentState: number, color: string, alpha: number): void;
 
         /**
-        * Disable windows 10 effect
+        * Disables support for Windows 10 effects (Acrylic and Blur)
         */
         public disableUser32(): void;
 
         /**
-        * Disable windows 11 effect
+        * Disables support for Windows 11 effects (Mica and Acrylic)
         */
         public disableDWM(): void;
 
         /**
-         * Execute function 'DwmSetWindowAttribute' from dwmapi.dll
+         * Executes the `DwmSetWindowAttribute` function from `dwmapi.dll`
          */
         public executeDwm(params: number, value: number): void;
 
         /**
-         * Execute function 'SetWindowCompositionAttribute' from user32.dll
+         * Executes the `SetWindowCompositionAttribute` function from `user32.dll`
          */
         public executeUser32(params: number, value: number): void;
     }
 
+    /**
+     * Shows if the machine is running Windows 11
+     */
     export const IS_WINDOWS_11: boolean;
+    /**
+     * Values specifying several window properties
+     */
     export const PARAMS: {
+        /**
+         * Effects configuration
+         */
         BACKGROUND: {
             AUTO: 0,
             NONE: 1,
-            ACRYLIC: 3,         // Acrylic
-            MICA: 2,            // Mica
-            TABBED_MICA: 4      // Mica tabbed
+            /**
+             * Acrylic Effect
+             */
+            ACRYLIC: 3,
+            /**
+             * Mica Effect
+             */
+            MICA: 2,
+            /**
+             * Mica Tabbed Effect
+             */
+            TABBED_MICA: 4
         },
+        /**
+         * Corner radius configuration
+         */
         CORNER: 5,
+        /**
+         * Border color configuration
+         */
         BORDER_COLOR: 6,
+        /**
+         * Titlebar color configuration
+         */
         CAPTION_COLOR: 7,
+        /**
+         * Text color configuration
+         */
         TEXT_COLOR: 8,
         FRAME: 9,
         MARGIN: 10
     };
+    /**
+     * Values which can be used in several effects
+     */
     export const VALUE: {
+        /**
+         * Theme values
+         */
         THEME: {
-            AUTO: 5,	// select theme by the windows theme
-            DARK: 1,	// select the dark theme
-            LIGHT: 2,	// select the white theme
+            /**
+             * Automatic theme
+             */
+            AUTO: 5,
+            /**
+             * Dark theme
+             */
+            DARK: 1,
+            /**
+             * Light theme
+             */
+            LIGHT: 2,
         },
+        /**
+         * Corner radius values
+         */
         CORNER: {
+            /**
+             * Default corners
+             */
             DEFAULT: 0,
+            /**
+             * Square corners
+             */
             DONOTROUND: 1,
+            /**
+             * Rounded corners
+             */
             ROUND: 2,
+            /**
+             * Small rounded corners
+             */
             ROUNDSMALL: 3
         },
+        /**
+         * Color values
+         */
         COLOR: {
             RED: 0x000000FF,
             GREEN: 0x0000FF00,
             BLUE: 0x00FF0000,
             BLACK: 0x00000000,
             WHITE: 0x00FFFFFF,
-            FROM_RGB: (r: number, g: number, b: number) => number
+            /**
+             * Convert red, green and blue values into a single hex color
+             */
+            FROM_RGB: (red: number, green: number, blue: number) => number
         },
+        /**
+         * Falsely value
+         */
         FALSE: 0,
+        /**
+         * Truthful value
+         */
         TRUE: 1
     };
+    /**
+     * Windows 10 effects
+     */
     export const WIN10: {
+        /**
+         * Transparent Effect
+         */
         TRANSPARENT: 2,
+        /**
+         * Blur Effect
+         */
         BLURBEHIND: 3,
+        /**
+         * Acrylic Effect
+         */
         ACRYLIC: 4
     }
 }
